861a17be378ef139a07b20afb4be7440441e272c
Nikolai Korolev <korolevns98@gmail.com>
2024-01-11 00:38:41 +0000

    Remove preloaded Sudoku

--- js/main.js
+++ js/main.js
@@ -375,17 +375,6 @@ function addSudokuTable() {
 
 function initialize() {
     addSudokuTable()
-    Sudoku.load([
-        [6, 1, 0, 0, 0, 0, 0, 5, 4],
-        [3, 0, 8, 0, 0, 4, 0, 0, 7],
-        [0, 0, 0, 0, 2, 0, 0, 1, 0],
-        [0, 8, 0, 0, 0, 0, 0, 0, 3],
-        [5, 0, 0, 2, 7, 0, 0, 0, 0],
-        [0, 0, 0, 8, 0, 1, 0, 7, 0],
-        [0, 0, 0, 0, 5, 0, 6, 3, 0],
-        [9, 5, 0, 1, 0, 6, 7, 4, 0],
-        [1, 7, 0, 4, 3, 2, 8, 9, 0]
-    ])
 }
 
 window.addEventListener("load", initialize)
